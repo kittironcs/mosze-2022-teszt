fe63bebd6af5d0e7af5b189b410a2aa8a46dbe50
kittironcs <73992247+kittironcs@users.noreply.github.com>
2022-09-19 22:26:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; /*NELEMENTS helyett N_ELEMENTS*/
+    std::cout << '1-100 ertekek duplazasa' /*aposztrófok helyett idézőjel*/
+    for (int i = 0;) /*ide még i = 0; után: i < N_ELEMENTS; i++ */
     {
         b[i] = i * 2;
     }
@@ -16,7 +16,7 @@ int main()
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) /*itt vessző helyett pontosvessző*/
     {
         atlag += b[i]
     }
