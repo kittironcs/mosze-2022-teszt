be493ffee94e37182685f81cc627abea479b521c
kittironcs <73992247+kittironcs@users.noreply.github.com>
2022-09-19 22:36:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; /*NELEMENTS helyett N_ELEMENTS*/
-    std::cout << '1-100 ertekek duplazasa' /*aposztrófok helyett idézőjel*/
-    for (int i = 0;) /*ide még i = 0; után: i < N_ELEMENTS; i++ */
+    int *b = new int[N_ELEMENTS]; /*NELEMENTS helyett N_ELEMENTS*/
+    std::cout << "1-100 ertekek duplazasa"; /*aposztrófok helyett idézőjel és pontosvessző*/
+    for (int i = 0;i < N_ELEMENTS; i++) /*ide még i = 0; után: i < N_ELEMENTS; i++ */
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"; /*sor végére pontosvessző*/
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) /*itt vessző helyett pontosvessző*/
+    for (int i = 0; i < N_ELEMENTS; i++) /*itt vessző helyett pontosvessző*/
     {
-        atlag += b[i]
+        atlag += b[i]; /*sor végére pontosvessző*/
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
